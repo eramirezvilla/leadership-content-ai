88afee1ee194690a2046095dac1cd35a6039bdeb
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-16 17:40:42 -0500

    initialize supabase client

---
+++ src/server/supabase.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
