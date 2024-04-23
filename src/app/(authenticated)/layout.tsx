e07b915122766461b9f297fc9dfc4d0799105e4f
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-23 15:46:58 -0500

    added sidebar to layout

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,7 +1,9 @@
+import SidebarNav from "~/components/ui/SidebarNav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
+      <SidebarNav/>
       <main className="w-full">{children}</main>
       </div>
   );
