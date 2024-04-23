33ae4153796a52be85b9308da46b6c578bca586e
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-23 15:40:05 -0500

    init dash

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton, useUser } from '@clerk/nextjs'
 
 export default function DashboardPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center">
+    <UserButton />
     </main>
   );
 }
