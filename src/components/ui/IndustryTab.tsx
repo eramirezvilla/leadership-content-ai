c97e143e161490e249b53fa885b8b1a1640049bd
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-25 17:12:39 -0500

    handle clicks to only show selected challenges

--- src/components/ui/IndustryTab.tsx
+++ src/components/ui/IndustryTab.tsx
@@ -3,19 +3,23 @@ import {type industry_challenge_mapping as indMap} from "@prisma/client"
 import { ChevronRightIcon } from '@radix-ui/react-icons'
 import { useState } from 'react'
 
+// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 type IndustryMap = {
-    mapping: indMap[]
+    [key: string]: Omit<indMap, 'industry_name'>[];
 }
 
-
 export default function IndustryTab({ industryMap }: { industryMap: IndustryMap }){
-    const [showChallenges, setShowChallenges] = useState(false)
+    const [selectedIndustry, setSelectedIndustry] = useState(null as string | null)
+
+    const handleIndustryClick = (industryName: string) => {
+        setSelectedIndustry(industryName)
+    }
 
     return (
         <div className="flex w-full pl-2">
             <div className="flex flex-col w-1/4 gap-2">
                     {Object.keys(industryMap).map((industryName) => (
-                        <div key={industryName} className="flex justify-between py-4 px-8 border border-1 rounded-md hover:bg-black/15 hover:cursor-pointer">
+                        <div key={industryName} onClick={() => handleIndustryClick(industryName)} className="flex justify-between py-4 px-8 border border-1 rounded-md hover:bg-black/15 hover:cursor-pointer">
                             <p className='w-prose'>{industryName}</p>
                             <div className="flex justify-end items-center">
                                 <ChevronRightIcon />
@@ -23,20 +27,17 @@ export default function IndustryTab({ industryMap }: { industryMap: IndustryMap 
                         </div>
                     ))}
             </div>
-            <div className="flex w-3/4">
-                {Object.entries(industryMap).map(([industryName, challenges]) => (
-                    <div key={industryName} className="flex flex-col items-center justify-center">
-                        <h1>{industryName}</h1>
+            <div className="flex flex-col w-3/4 p-4">
+                {selectedIndustry && (
+                    <div className="flex flex-col items-center justify-center">
+                        <h1>{selectedIndustry}</h1>
                         <ul>
-                            {challenges.map((challenge) => (
-                                <>
-                                <li key={challenge.discussion_topic}>{challenge.discussion_topic}</li>
-                                <li key={challenge.topic_description}>{challenge.topic_description}</li>
-                                </>
+                            {industryMap[selectedIndustry]!.map((challenge, index) => (
+                                <li key={index}>{challenge.discussion_topic} - {challenge.topic_description}</li>
                             ))}
                         </ul>
                     </div>
-                ))}
+                )}
             </div>
         </div>
     )
