fee3f65525b274706e9f368edb51e1a711e6f333
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-25 17:26:28 -0500

    added apple hic guides for text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,19 @@ const config = {
       },
     },
     extend: {
+      fontSize: {
+        large_title: ["34.58px", "42.56px"],
+        title_1: ["29.26px", "34.58px"],
+        title_2: ["22.61px", "29.26px"],
+        title_3: ["19.95px", "26.6px"],
+        headline: ["17.29px", "21.28px"],
+        body: ["17.29px", "21.28px"],
+        callout: ["15.96px", "19.95px"],
+        subheadline: ["14.63px", "18.62px"],
+        footnote: ["13.3px", "17.29px"],
+        caption_1: ["13.3px", "17.29px"],
+        caption_2: ["13.3px", "17.29px"],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
