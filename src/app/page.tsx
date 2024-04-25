1787032208f4118915658b1e81aefb3ce97d2335
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-25 17:03:52 -0500

    redirect if already signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import { Button } from "~/components/ui/button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs"
 
 export default function HomePage() {
+
+  const { userId } = auth();
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <Link href="/sign-in">
