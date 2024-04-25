129e5acd67d68efaffbbaccaec54434ac3996e6f
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-25 17:18:06 -0500

    formatting

--- src/components/ui/IndustryTab.tsx
+++ src/components/ui/IndustryTab.tsx
@@ -1,44 +1,56 @@
-"use client"
-import {type industry_challenge_mapping as indMap} from "@prisma/client"
-import { ChevronRightIcon } from '@radix-ui/react-icons'
-import { useState } from 'react'
+"use client";
+import { type industry_challenge_mapping as indMap } from "@prisma/client";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 type IndustryMap = {
-    [key: string]: Omit<indMap, 'industry_name'>[];
-}
+  [key: string]: Omit<indMap, "industry_name">[];
+};
 
-export default function IndustryTab({ industryMap }: { industryMap: IndustryMap }){
-    const [selectedIndustry, setSelectedIndustry] = useState(null as string | null)
+export default function IndustryTab({
+  industryMap,
+}: {
+  industryMap: IndustryMap;
+}) {
+  const [selectedIndustry, setSelectedIndustry] = useState(
+    null as string | null,
+  );
 
-    const handleIndustryClick = (industryName: string) => {
-        setSelectedIndustry(industryName)
-    }
+  const handleIndustryClick = (industryName: string) => {
+    setSelectedIndustry(industryName);
+  };
 
-    return (
-        <div className="flex w-full pl-2">
-            <div className="flex flex-col w-1/4 gap-2">
-                    {Object.keys(industryMap).map((industryName) => (
-                        <div key={industryName} onClick={() => handleIndustryClick(industryName)} className="flex justify-between py-4 px-8 border border-1 rounded-md hover:bg-black/15 hover:cursor-pointer">
-                            <p className='w-prose'>{industryName}</p>
-                            <div className="flex justify-end items-center">
-                                <ChevronRightIcon />
-                            </div>
-                        </div>
-                    ))}
+  return (
+    <div className="flex w-full pl-2">
+      <div className="flex w-1/4 flex-col gap-2">
+        {Object.keys(industryMap).map((industryName) => (
+          <div
+            key={industryName}
+            onClick={() => handleIndustryClick(industryName)}
+            className={`border-1 flex justify-between rounded-md border px-8 py-4 hover:cursor-pointer hover:bg-black/15 ${selectedIndustry === industryName ? "bg-orange-500/50 hover:bg-orange-500/15" : ""}`}
+          >
+            <p className="w-prose">{industryName}</p>
+            <div className="flex items-center justify-end">
+              <ChevronRightIcon />
             </div>
-            <div className="flex flex-col w-3/4 p-4">
-                {selectedIndustry && (
-                    <div className="flex flex-col items-center justify-center">
-                        <h1>{selectedIndustry}</h1>
-                        <ul>
-                            {industryMap[selectedIndustry]!.map((challenge, index) => (
-                                <li key={index}>{challenge.discussion_topic} - {challenge.topic_description}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+      <div className="flex w-3/4 flex-col p-4">
+        {selectedIndustry && (
+          <div className="flex flex-col items-center justify-center">
+            <h1>{selectedIndustry}</h1>
+            <ul>
+              {industryMap[selectedIndustry]!.map((challenge, index) => (
+                <li key={index}>
+                  {challenge.discussion_topic} - {challenge.topic_description}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
