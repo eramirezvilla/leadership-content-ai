99c7d5693360038ee44178005564dc7aa4ee5ec2
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-25 17:04:30 -0500

    moving industry tab to component

--- src/app/(authenticated)/industries/page.tsx
+++ src/app/(authenticated)/industries/page.tsx
@@ -1,5 +1,7 @@
 import prisma from '~/server/prisma'
 import { type industry_challenge_mapping } from '@prisma/client';
+import { ChevronRightIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 type IndustryMap = {
@@ -20,20 +22,47 @@ export default async function IndustriesPage() {
     }, {});
 
     return (
-        <div className="flex flex-col w-full items-center justify-center">
-            {Object.entries(industryMap).map(([industryName, challenges]) => (
-                <div key={industryName} className="flex flex-col items-center justify-center">
-                    <h1>{industryName}</h1>
-                    <ul>
-                        {challenges.map((challenge) => (
-                            <>
-                            <li key={challenge.discussion_topic}>{challenge.discussion_topic}</li>
-                            <li key={challenge.topic_description}>{challenge.topic_description}</li>
-                            </>
-                        ))}
-                    </ul>
+        <div className="flex w-full pl-2">
+            <div className="flex flex-col w-1/4 gap-2">
+                    {Object.keys(industryMap).map((industryName) => (
+                        <div key={industryName} className="flex justify-between py-4 px-8 border border-1 rounded-md hover:bg-black/15 hover:cursor-pointer">
+                            <p className='w-prose'>{industryName}</p>
+                            <div className="flex justify-end items-center">
+                                <ChevronRightIcon />
+                            </div>
+                        </div>
+                    ))}
+            </div>
+            <div className="flex w-3/4">
+                {Object.entries(industryMap).map(([industryName, challenges]) => (
+                    <div key={industryName} className="flex flex-col items-center justify-center">
+                        <h1>{industryName}</h1>
+                        <ul>
+                            {challenges.map((challenge) => (
+                                <>
+                                <li key={challenge.discussion_topic}>{challenge.discussion_topic}</li>
+                                <li key={challenge.topic_description}>{challenge.topic_description}</li>
+                                </>
+                            ))}
+                        </ul>
                     </div>
-            ))}
+                ))}
+            </div>
         </div>
+        // <div className="flex flex-col w-full items-center justify-center">
+        //     {Object.entries(industryMap).map(([industryName, challenges]) => (
+        //         <div key={industryName} className="flex flex-col items-center justify-center">
+        //             <h1>{industryName}</h1>
+        //             <ul>
+        //                 {challenges.map((challenge) => (
+        //                     <>
+        //                     <li key={challenge.discussion_topic}>{challenge.discussion_topic}</li>
+        //                     <li key={challenge.topic_description}>{challenge.topic_description}</li>
+        //                     </>
+        //                 ))}
+        //             </ul>
+        //             </div>
+        //     ))}
+        // </div>
     )
 }
