b83d9d30d63e452bfeb0bfdbd726e4ffec386815
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-26 01:35:22 -0500

    updated validation

--- src/lib/validation/IndustryChallenge.tsx
+++ src/lib/validation/IndustryChallenge.tsx
@@ -4,12 +4,9 @@ import { z } from "zod";
 export const createIndustrySchema = z.object({
     industry_name: z
     .string()
-    .min(3, { message: "Title must be at least 3 characters long" })
     .optional(),
     discussion_topic: z.string().optional(),
-    topic_description: z.string().optional(),
-    source_file_id: z.number().optional(),
-    created_at: z.date()
+    topic_description: z.string().optional()
 });
 
 export type CreateIndustrySchema = z.infer<typeof createIndustrySchema>;
@@ -17,7 +14,6 @@ export type CreateIndustrySchema = z.infer<typeof createIndustrySchema>;
 export const updateIndustrySchema = createIndustrySchema.extend({
   id: z.string().min(1),
 });
-export type UpdateIndustrySchema = z.infer<typeof updateIndustrySchema>;
 
 export const deleteIndustrySchema = z.object({
   id: z.string().min(1),
