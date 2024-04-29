7745dc1f1662e388e6612a76570e656d16f55d80
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 11:59:41 -0500

    updated component call

--- src/app/(authenticated)/posts/page.tsx
+++ src/app/(authenticated)/posts/page.tsx
@@ -1,14 +1,16 @@
 import prisma from "~/lib/server/prisma"
 import ExistingPostTab from "~/components/ui/ExistingPostTab"
+import AddPost from "~/components/ui/AddPost"
 
 export default async function PostsPage() {
     const allPosts = await prisma.post.findMany()
+    const allThemes = await prisma.themes.findMany()
+    const allIndustries = await prisma.industry_challenge_mapping.findMany()
 
     return (
         <div>
-            {allPosts.length > 0 ? 
-            <ExistingPostTab allPosts={allPosts} />
-            : <h1>No posts found</h1>}
+            <ExistingPostTab allPosts={allPosts} allThemes={allThemes} allIndustries={allIndustries} />
+
         </div>
     )
 }
