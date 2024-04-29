70c81a714f0890f6bf13d4552ef14033c962d3fc
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 11:59:25 -0500

    add button to open create new post dialog

--- src/components/ui/ExistingPostTab.tsx
+++ src/components/ui/ExistingPostTab.tsx
@@ -1,20 +1,35 @@
-"use client"
-import { type post } from "@prisma/client"
+"use client";
+import { type post, themes, industry_challenge_mapping } from "@prisma/client";
+import AddPost from "./AddPost";
+import { Button } from "./button";
+import { useState } from "react";
 
 interface ExistingPostTabProps {
-    allPosts: post[]
+  allPosts: post[];
+    allThemes: themes[];
+    allIndustries: industry_challenge_mapping[];
 }
 
-export default function ExistingPostTab({allPosts} : ExistingPostTabProps) {
-    return (
-        <div>
-            <h1>Posts</h1>
-            {allPosts.map((post) => (
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.content}</p>
-                </div>
-            ))}
-        </div>
-    )
+export default function ExistingPostTab({ allPosts, allThemes, allIndustries}: ExistingPostTabProps) {
+    const [showAddDialog, setShowAddDialog] = useState(false);
+  return (
+    <div>
+      <h1>Posts</h1>
+      {allPosts.length > 0 ? (
+        allPosts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </div>
+        ))
+      ) : (
+        <>
+        <Button type="button" onClick={() => setShowAddDialog(true)}>
+            Add Post
+        </Button>
+        <AddPost open={showAddDialog} setOpen={setShowAddDialog} allIndustries={allIndustries} allThemes={allThemes} />
+        </>
+      )}
+    </div>
+  );
 }
