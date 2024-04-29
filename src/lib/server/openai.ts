6bd526fceaf694794404a79615a8d6d6b0f5441a
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 10:30:29 -0500

    create openai getEmbedding function

---
+++ src/lib/server/openai.ts
@@ -0,0 +1,26 @@
+import OpenAi from "openai";
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+  throw new Error("No OpenAI API key found");
+}
+
+const openai = new OpenAi({ apiKey });
+
+export default openai;
+
+export async function getEmbedding(text: string) {
+    const response = await openai.embeddings.create({
+        model: "text-embedding-3-large",
+        input: text,
+    });
+
+    const embedding = response.data[0]?.embedding;
+    if (!embedding) {
+        throw new Error("No embedding found");
+    }
+
+    console.log(embedding);
+    return embedding;
+}
