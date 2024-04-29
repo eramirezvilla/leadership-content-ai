7c15d4ae9e1192208a720ba6f064ead0be2191b5
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 11:10:18 -0500

    display existing posts

--- src/app/(authenticated)/posts/page.tsx
+++ src/app/(authenticated)/posts/page.tsx
@@ -1,11 +1,14 @@
 import prisma from "~/lib/server/prisma"
+import ExistingPostTab from "~/components/ui/ExistingPostTab"
 
 export default async function PostsPage() {
     const allPosts = await prisma.post.findMany()
 
     return (
         <div>
-            <h1>Posts</h1>
+            {allPosts.length > 0 ? 
+            <ExistingPostTab allPosts={allPosts} />
+            : <h1>No posts found</h1>}
         </div>
     )
 }
