29c04ea48e07412c1e209c9047170f4827050fae
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 10:25:21 -0500

    create pinecone client

---
+++ src/lib/server/pinecone.ts
@@ -0,0 +1,14 @@
+import { Pinecone } from "@pinecone-database/pinecone"
+
+const apiKey = process.env.PINECONE_API_KEY;
+
+if(!apiKey) {
+  throw new Error('No Pinecone API key found');
+}
+
+const pinecone = new Pinecone({ 
+    apiKey
+});
+
+export const notesIndex = pinecone.Index("i-forgot");
+
