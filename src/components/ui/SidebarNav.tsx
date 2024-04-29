7a758a78679be1d6e755fbdbb20517780db35f08
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 10:17:16 -0500

    handling bg color changes on click

--- src/components/ui/SidebarNav.tsx
+++ src/components/ui/SidebarNav.tsx
@@ -1,9 +1,17 @@
 "use client";
 import Link from "next/link";
 import { useUser, UserButton } from "@clerk/nextjs";
+import { useState } from "react";
 
 export default function SidebarNav() {
   const { user } = useUser();
+
+  const [selectedItem, setSelectedItem] = useState("");
+
+  const handleItemClick = (item: string) => {
+    setSelectedItem(item);
+  };
+
   return (
     <div className="sticky left-0 top-0 flex h-screen w-60 min-w-60 border-r">
       <div className="flex w-full flex-col pt-4">
@@ -16,25 +24,33 @@ export default function SidebarNav() {
             <h2 className="text-xs font-medium">Import</h2>
           </div> */}
           <Link href="/posts">
-            <div className="flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15">
+            <div 
+            onClick={() => handleItemClick("Posts")}
+            className={`flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Posts" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
               {/* <FileText size={24} /> */}
               <h3 className="text-sm font-medium">Posts</h3>
             </div>
           </Link>
           <Link href="/themes">
-            <div className="flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15">
+            <div 
+            onClick={() => handleItemClick("Themes")}
+            className={`flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Themes" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
               {/* <TextSearch size={24} /> */}
               <h3 className="text-sm font-medium">Themes</h3>
             </div>
           </Link>
           <Link href="/industries">
-            <div className="flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15">
+            <div 
+            onClick={() => handleItemClick("Industries")}
+            className={`flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Industries" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
               {/* <TextSearch size={24} /> */}
               <h3 className="text-sm font-medium">Industries</h3>
             </div>
           </Link>
           <Link href="/assets">
-            <div className="flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15">
+            <div 
+            onClick={() => handleItemClick("Assets")}
+            className={`flex min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Assets" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
               {/* <TextSearch size={24} /> */}
               <h3 className="text-sm font-medium">Assets</h3>
             </div>
