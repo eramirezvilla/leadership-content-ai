87490816e3a42e753b0d789a7596921531974911
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 10:16:52 -0500

    added themes page

--- src/app/(authenticated)/themes/page.tsx
+++ src/app/(authenticated)/themes/page.tsx
@@ -1,6 +1,14 @@
+import prisma from "~/lib/server/prisma"
+import { useState } from "react"
+import { type themes } from "@prisma/client"
+import ThemeTab from "~/components/ui/ThemeTab"
 
+export default async function ThemesPage() {
+    const allThemes = await prisma.themes.findMany()
 
-export default function ThemesPage() {
-    return <>
-    </>
+    return (
+        <div className="flex w-full">
+            <ThemeTab allThemes={allThemes} />
+        </div>
+    )
 }
