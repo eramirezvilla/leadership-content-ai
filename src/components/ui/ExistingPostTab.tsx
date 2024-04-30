980fc140e37e53c803ab3da16572b42159b48be8
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-29 22:50:22 -0500

    format posts

--- src/components/ui/ExistingPostTab.tsx
+++ src/components/ui/ExistingPostTab.tsx
@@ -6,30 +6,45 @@ import { useState } from "react";
 
 interface ExistingPostTabProps {
   allPosts: post[];
-    allThemes: themes[];
-    allIndustries: industry_challenge_mapping[];
+  allThemes: themes[];
+  allIndustries: industry_challenge_mapping[];
 }
 
-export default function ExistingPostTab({ allPosts, allThemes, allIndustries}: ExistingPostTabProps) {
-    const [showAddDialog, setShowAddDialog] = useState(false);
+export default function ExistingPostTab({
+  allPosts,
+  allThemes,
+  allIndustries,
+}: ExistingPostTabProps) {
+  const [showAddDialog, setShowAddDialog] = useState(false);
   return (
-    <div>
-      <h1>Posts</h1>
-      {allPosts.length > 0 ? (
-        allPosts.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-          </div>
-        ))
-      ) : (
-        <>
+    <div className="flex flex-col gap-4">
+      <h1 className=" ml-8 text-title_2">Posts</h1>
+      <div className="flex w-full">
+        {allPosts.length > 0 ? (
+          allPosts.map((post) => (
+            <div
+              className="border-1 flex max-w-prose flex-col gap-2 rounded-md border px-8 py-4"
+              key={post.id}
+            >
+              <h2 className="text-title_3">{post.title}</h2>
+              <p className="text-callout">{post.content}</p>
+            </div>
+          ))
+        ) : (
+          <p>No posts found</p>
+        )}
+      </div>
+      <div className="flex w-full items-center justify-center">
         <Button type="button" onClick={() => setShowAddDialog(true)}>
-            Add Post
+          Add Post
         </Button>
-        <AddPost open={showAddDialog} setOpen={setShowAddDialog} allIndustries={allIndustries} allThemes={allThemes} />
-        </>
-      )}
+        <AddPost
+          open={showAddDialog}
+          setOpen={setShowAddDialog}
+          allIndustries={allIndustries}
+          allThemes={allThemes}
+        />
+      </div>
     </div>
   );
 }
