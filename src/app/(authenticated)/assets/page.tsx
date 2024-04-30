e75c29a29d60f3ffac9fffbf008e2f08d4abd6ce
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-30 17:48:05 -0500

    display all files

--- src/app/(authenticated)/assets/page.tsx
+++ src/app/(authenticated)/assets/page.tsx
@@ -1,6 +1,24 @@
+import prisma from "~/lib/server/prisma"
 
+export default async function AssetPage() {
 
-export default function AssetPage() {
-    return <>
-    </>
+    const allFiles = await prisma.file.findMany()
+    allFiles.map((file) => {
+        console.log(file)
+    })
+
+    return (
+        <div className="grid">
+            <h1>Assets</h1>
+            <div className="grid grid-cols-3 gap-4">
+                {allFiles.map((file) => {
+                    return (
+                        <div key={file.id} className="bg-gray-100 p-4">
+                            <h2>{file.filename}</h2>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
