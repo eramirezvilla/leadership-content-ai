7eef7b8214f0be80c7221507998e118b07e8ca2c
eramirezvilla <eramirezvilla@sandiego.edu>
2024-04-30 18:29:29 -0500

    updated env vars

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_PUBLIC_API_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
