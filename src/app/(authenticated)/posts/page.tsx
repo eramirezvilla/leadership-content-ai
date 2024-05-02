79a36842727d8ad833a4d28317411a92de9234e7
eramirezvilla <eramirezvilla@sandiego.edu>
2024-05-02 18:14:01 -0500

    only show the user's posts

--- src/app/(authenticated)/posts/page.tsx
+++ src/app/(authenticated)/posts/page.tsx
@@ -1,26 +1,35 @@
-import prisma from "~/lib/server/prisma"
-import AddPost from "~/components/ui/AddPost"
+import prisma from "~/lib/server/prisma";
+import AddPost from "~/components/ui/AddPost";
 import type { industry_challenge_mapping } from "@prisma/client";
+import { auth } from "@clerk/nextjs";
 
 export default async function PostsPage() {
-    const allPosts = await prisma.post.findMany()
-    const allThemes = await prisma.themes.findMany()
-    const allIndustries = await prisma.industry_challenge_mapping.findMany()
+  const { userId } = auth();
+  if (!userId) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const allPosts = await prisma.post.findMany({
+    where: {
+      user_id: userId,
+    },
+  });
+  const allThemes = await prisma.themes.findMany();
+  const allIndustries = await prisma.industry_challenge_mapping.findMany();
 
-    return (
-        <div className="flex flex-col w-full my-8 gap-4">
-            <div className="flex w-1/4 justify-center gap-6">
-                <h1 className="text-title_2">Posts</h1>
-                <AddPost allThemes={allThemes} allIndustries={allIndustries} />
-            </div>
-            <div className="flex w-full flex-wrap gap-4 px-4">
+  return (
+    <div className="my-8 flex w-full flex-col gap-4">
+      <div className="flex w-1/4 justify-center gap-6">
+        <h1 className="text-title_2">Posts</h1>
+        <AddPost allThemes={allThemes} allIndustries={allIndustries} />
+      </div>
+      <div className="flex w-full flex-wrap gap-4 px-4">
         {allPosts.length > 0 ? (
           allPosts.map((post) => (
             <div
               className="border-1 flex w-96 flex-col gap-2 rounded-md border bg-black/5 px-8 py-4"
               key={post.id}
             >
-              <div className="border-1 h-20 overflow-auto flex w-full rounded-md border bg-white px-4 py-2">
+              <div className="border-1 flex h-20 w-full overflow-auto rounded-md border bg-white px-4 py-2">
                 <h2 className="text-headline">{post.title}</h2>
               </div>
               <div className="border-1 flex w-full rounded-md border bg-white px-4 py-2">
@@ -28,7 +37,7 @@ export default async function PostsPage() {
                   {post.content}
                 </p>
               </div>
-              <div className="border-1 h-40 overflow-auto flex w-full rounded-md border bg-white px-4 py-2">
+              <div className="border-1 flex h-40 w-full overflow-auto rounded-md border bg-white px-4 py-2">
                 {(() => {
                   const industryMapping = getIndustryMapping(
                     Number(post.created_from_mapping),
@@ -47,44 +56,51 @@ export default async function PostsPage() {
                   );
                 })()}
               </div>
-            <div className="border-1 flex w-full rounded-md border bg-white px-4 py-2">
+              <div className="border-1 flex w-full rounded-md border bg-white px-4 py-2">
                 {(async () => {
-                    const relevantFiles = getRelevantFiles(post.relevant_files as number[]);
-                    if (relevantFiles !== null && relevantFiles !== undefined) {
-                        return (
-                            <p className="max-w-lg text-body">
-                                Relevant Files: {(await relevantFiles).map((file) => file.filename)}
-                            </p>
-                        );
-                    } else {
-                        return <p className="max-w-lg text-body">No relevant files found</p>;
-                    }
+                  const relevantFiles = getRelevantFiles(
+                    post.relevant_files as number[],
+                  );
+                  if (relevantFiles !== null && relevantFiles !== undefined) {
+                    return (
+                      <p className="max-w-lg text-body">
+                        Relevant Files:{" "}
+                        {(await relevantFiles).map((file) => file.filename)}
+                      </p>
+                    );
+                  } else {
+                    return (
+                      <p className="max-w-lg text-body">
+                        No relevant files found
+                      </p>
+                    );
+                  }
                 })()}
-            </div>
+              </div>
             </div>
           ))
         ) : (
           <p>No posts found</p>
         )}
       </div>
-        </div>
-    )
+    </div>
+  );
 }
 
 export function getIndustryMapping(
-    id: number,
-    allIndustries: industry_challenge_mapping[],
-  ) {
-    return allIndustries.find((industry) => Number(industry.id) === id);
-  }
+  id: number,
+  allIndustries: industry_challenge_mapping[],
+) {
+  return allIndustries.find((industry) => Number(industry.id) === id);
+}
 
 export async function getRelevantFiles(ids: number[]) {
-    const relevantFiles = await prisma.file.findMany({
-        where: {
-            id: {
-                in: ids
-            }
-        }
-    })
-    return relevantFiles
+  const relevantFiles = await prisma.file.findMany({
+    where: {
+      id: {
+        in: ids,
+      },
+    },
+  });
+  return relevantFiles;
 }
