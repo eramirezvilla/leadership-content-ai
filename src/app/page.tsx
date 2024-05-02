98b06aa1fda06b15dc2d0b9e4d349d10f34ea372
eramirezvilla <eramirezvilla@sandiego.edu>
2024-05-02 13:39:32 -0500

    updated messaging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,14 @@ export default function HomePage() {
       <div className="relative z-20 flex h-full w-full flex-col items-center justify-center gap-8">
         <Image src={logo} alt="Capo logo" width={400} height={400} />
         <div className="flex flex-col items-center justify-center gap-6 w-full">
-          <p className="max-w-prose text-lg font-semibold text-white">
-            AI generated content designed for your audience using your content.
-          </p>
+          <div className="flex flex-col items-start gap-4">
+            <p className="max-w-[450px] text-xl font-black text-white justify-center">
+              Get more from the data you already have.
+            </p>
+            <p className="font-medium text-base text-white justify-center max-w-[400px]">
+              Use your company&apos;s existing data to generate social media posts, blogs, and more.
+            </p>
+          </div>
           <div className="flex w-full justify-center">
             <Link href="/sign-in">
               <Button type="button" className="bg-black/50 text-white font-semibold hover:bg-white/10 text-lg">Sign In</Button>
