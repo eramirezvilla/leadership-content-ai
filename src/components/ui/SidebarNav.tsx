bda9fc7d4b6c8a5fe92ebddc0ef0dfb5d5c3000e
eramirezvilla <eramirezvilla@sandiego.edu>
2024-05-07 11:44:47 -0500

    updated navigation sections

--- src/components/ui/SidebarNav.tsx
+++ src/components/ui/SidebarNav.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { useUser, UserButton } from "@clerk/nextjs";
 import { useState } from "react";
-import {Building2, FilePen, Folder, ListChecks} from "lucide-react"
+import {Building2, FilePen, Folder, ListChecks, ChevronRight, Ellipsis} from "lucide-react"
 
 export default function SidebarNav() {
   const { user } = useUser();
@@ -14,43 +14,63 @@ export default function SidebarNav() {
   };
 
   return (
-    <div className="sticky left-0 top-0 flex h-screen w-40 min-w-40 border-r">
-      <div className="flex w-full flex-col pt-4 gap-8 mt-4">
+    <div className="sticky left-0 top-0 flex h-screen w-80 min-w-40 border-r">
+      <div className="flex w-full flex-col pt-4 px-4 gap-8 mt-4">
         <div className="flex flex-col items-center justify-center gap-2 object-cover">
           <UserButton />
           <h1 className="text-md font-medium">Welcome, {user?.firstName} </h1>
         </div>
         <div className="flex w-full flex-col gap-4 px-2 pt-2">
+          <div className="flex w-full justify-start items-center gap-2.5 pl-2.5">
+            <Ellipsis size={12} />
+            <h1 className="text-xs font-medium text-black/50">Content</h1>
+          </div>
           <Link href="/posts">
             <div 
             onClick={() => handleItemClick("Posts")}
-            className={`flex flex-col min-h-4 items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Posts" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
-              <FilePen size={24} />
-              <h3 className="text-sm font-medium">Posts</h3>
+            className={`flex min-h-4 items-center justify-between hover:text-brand_purple rounded-xl pl-2.5 py-2 ${selectedItem === "Posts" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>              
+                <div className="flex items-center justify-start text-subheadline gap-2.5">
+                  <FilePen size={24} />
+                  <h3 className="text-sm font-medium">Posts</h3>
+                </div>
+                <ChevronRight size={24} />
             </div>
           </Link>
           <Link href="/themes">
             <div 
             onClick={() => handleItemClick("Themes")}
-            className={`flex flex-col min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Themes" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
-              <ListChecks size={24} />
-              <h3 className="text-sm font-medium">Themes</h3>
+            className={`flex min-h-4 items-center justify-between hover:text-brand_purple rounded-xl pl-2.5 py-2 ${selectedItem === "Themes" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
+              <div className="flex items-center justify-start text-subheadline gap-2.5">
+                <ListChecks size={24} />
+                <h3 className="text-sm font-medium">Themes</h3>
+              </div>
+              <ChevronRight size={24} />
             </div>
           </Link>
+          <div className="flex w-full justify-start items-center mt-2 gap-2.5 pl-2.5">
+            <Ellipsis size={12} />
+            <h1 className="text-xs font-medium text-black/50">Data</h1>
+          </div>
           <Link href="/industries">
             <div 
             onClick={() => handleItemClick("Industries")}
-            className={`flex flex-col min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Industries" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
-              <Building2 size={24} />
-              <h3 className="text-sm font-medium">Industries</h3>
+            className={`flex min-h-4 items-center justify-between hover:text-brand_purple rounded-xl pl-2.5 py-2 ${selectedItem === "Industries" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
+              <div className="flex items-center justify-start text-subheadline gap-2.5">
+                <Building2 size={24} />
+                <h3 className="text-sm font-medium">Industries</h3>
+              </div>
+              <ChevronRight size={24} />
             </div>
           </Link>
           <Link href="/assets">
             <div 
             onClick={() => handleItemClick("Assets")}
-            className={`flex flex-col min-h-4 w-full items-center justify-center gap-2.5 rounded-xl px-2.5 py-2 outline outline-1 outline-black hover:bg-black/15 ${selectedItem === "Assets" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
-              <Folder size={24} />
-              <h3 className="text-sm font-medium">Assets</h3>
+            className={`flex min-h-4 items-center justify-between hover:text-brand_purple rounded-xl pl-2.5 py-2 ${selectedItem === "Assets" ? "bg-orange/50 hover:bg-orange/500" : ""}`}>
+              <div className="flex items-center justify-start text-subheadline gap-2.5">
+                <Folder size={24} />
+                <h3 className="text-sm font-medium">Assets</h3>
+              </div>
+              <ChevronRight size={24} />
             </div>
           </Link>
         </div>
