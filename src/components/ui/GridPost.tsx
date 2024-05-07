444cccc9499572a1241dbea16e3b5451ef14f8ff
eramirezvilla <eramirezvilla@sandiego.edu>
2024-05-07 17:45:13 -0500

    trigger post modal

--- src/components/ui/GridPost.tsx
+++ src/components/ui/GridPost.tsx
@@ -1,17 +1,35 @@
-import { type post} from '@prisma/client'
-import { Clock } from "lucide-react"
+"use client";
+import { type post } from "@prisma/client";
+import { Clock } from "lucide-react";
+import { useState } from "react";
+import PostModal from "./PostModal";
 
-export default function GridPost({ post }: { post: post }){
-    const { title, content, created_at } = post;
+export default function GridPost({ post }: { post: post }) {
+  const { title, content, created_at } = post;
+  const [showEditModal, setShowEditModal] = useState(false);
 
-    return (
-        <div className="flex flex-col items-start w-72 max-h-52 border gap-2 border-black rounded-lg px-4 py-2">
-            <div className="flex gap-2.5 items-center justify-center">
-                <Clock size={16} />
-                <p className='text-sm font-medium text-black/50'>{created_at.toLocaleDateString("en-US")}</p>
-            </div>
-            <h1 className='text-sm font-semibold'>{title}</h1>
-            <p className='text-xs font-normal text-black/50 whitespace-break-spaces overflow-scroll'>{content}</p>
+  return (
+    <>
+      <div
+        className="flex max-h-52 w-72 flex-col items-start gap-2 rounded-lg border border-black px-4 py-2 hover:cursor-pointer"
+        onClick={() => setShowEditModal(true)}
+      >
+        <div className="flex items-center justify-center gap-2.5">
+          <Clock size={16} />
+          <p className="text-sm font-medium text-black/50">
+            {created_at.toLocaleDateString("en-US")}
+          </p>
         </div>
-    )
+        <h1 className="text-sm font-semibold">{title}</h1>
+        <p className="overflow-scroll whitespace-break-spaces text-xs font-normal text-black/50">
+          {content}
+        </p>
+      </div>
+      <PostModal
+        open={showEditModal}
+        setOpen={setShowEditModal}
+        postToEdit={post}
+      ></PostModal>
+    </>
+  );
 }
