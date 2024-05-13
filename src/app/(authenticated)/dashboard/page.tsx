0a234042db32974f887ec3a7cef7dbb281fbbc4a
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-13 14:25:16 -0500

    test the new calendar

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton, useUser } from '@clerk/nextjs'
+import CalendarTest from '~/components/ui/CalendarTest';
 
 export default function DashboardPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
     <UserButton />
+    <CalendarTest />  
     </main>
   );
 }
