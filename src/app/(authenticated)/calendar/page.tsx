5a2d448d93ed8f554a2182944c5d2c7a88296da5
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-13 14:43:39 -0500

    create calendar route

---
+++ src/app/(authenticated)/calendar/page.tsx
@@ -0,0 +1,10 @@
+import CalendarTest from "~/components/ui/CalendarTest"
+
+export default function Calendar(){
+
+    return (
+        <div className="flex w-full h-full justify-center items-center">
+        <CalendarTest />  
+        </div>
+    )
+}
