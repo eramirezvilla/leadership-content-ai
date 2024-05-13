16d24d5c01f9df2dbf26d7244235961cd893597e
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-13 14:24:57 -0500

    testing calendar from template

---
+++ src/components/ui/CalendarTest.tsx
@@ -0,0 +1,36 @@
+"use client"
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction"
+import BootstrapTheme from "@fullcalendar/bootstrap"
+import listPlugin from '@fullcalendar/list';
+
+export default function CalendarTest() {
+
+    return (
+        <FullCalendar
+                plugins={[
+                  BootstrapTheme,
+                  dayGridPlugin,
+                  interactionPlugin,
+                  listPlugin
+                ]}
+                slotDuration={"00:15:00"}
+                handleWindowResize={true}
+                themeSystem="bootstrap"
+                headerToolbar={{
+                  left: "prev,next today",
+                  center: "title",
+                  right: "dayGridMonth,dayGridWeek,dayGridDay,listWeek",
+                }}
+                events={[
+                    { title: 'event 1', date: '2019-04-01' },
+                    { title: 'event 2', date: '2019-04-02' }
+                  ]}
+                editable={true}
+                droppable={true}
+                selectable={true}
+                initialView="dayGridMonth"
+              />
+    );
+    }
