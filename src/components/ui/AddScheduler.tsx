5b7bf9110c7095846649d1e6a69e641ad385a204
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-14 16:33:01 -0500

    inital form

--- src/components/ui/AddScheduler.tsx
+++ src/components/ui/AddScheduler.tsx
@@ -1,37 +1,50 @@
-"use client"
+"use client";
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-  } from "./dialog";
-import { type scheduler } from "@prisma/client"
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./dialog";
+import { type scheduler } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "./form";
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "./button";
 import { cn } from "~/lib/utils";
-import {type CreateScheduleSchema, createScheduleSchema} from "~/lib/validation/Scheduler";
+import {
+  type CreateScheduleSchema,
+  createScheduleSchema,
+} from "~/lib/validation/Scheduler";
 
 interface SchedulerModalProps {
-    schedulerToEdit?: scheduler;
-    open: boolean;
-    setOpen: (open: boolean) => void;
+  schedulerToEdit?: scheduler;
+  open: boolean;
+  setOpen: (open: boolean) => void;
 }
 
-export default function AddScheduler({ schedulerToEdit, open, setOpen }: SchedulerModalProps){
+export default function AddScheduler({
+  schedulerToEdit,
+  open,
+  setOpen,
+}: SchedulerModalProps) {
+  const form = useForm<CreateScheduleSchema>({
+    resolver: zodResolver(createScheduleSchema),
+    defaultValues: {
+      title: "",
+      item_type: "",
+      start_from: new Date(),
+      end_on: undefined,
+      frequency: [false, false, false, false, false, false, false],
+    },
+  });
 
-    return (
-        <>
-        </>
-    )
-
+  return <></>;
 }
