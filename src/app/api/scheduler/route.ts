371b0e889c3cd0baac6f956c9c5ead8feb27faae
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-16 15:20:24 -0500

    init scheduler api

--- src/app/api/scheduler/route.ts
+++ src/app/api/scheduler/route.ts
@@ -15,12 +15,35 @@ export async function POST(req: Request) {
       return Response.json({ error: "Invalid input" }, { status: 400 });
     }
 
+    // create schedule in database
+    await prisma.scheduler.create({
+      data: parseResult.data,
+    });
 
-    console.log("submitted data: ", parseResult.data);
+    // generate posts for the schedule with schedule_date between the start_from and end_on dates, according to the frequency
 
-    // await prisma.scheduler.create({
-    //   data: parseResult.data,
-    // });
+        const currentDate = parseResult.data.start_from;
+        while (currentDate <= parseResult.data.end_on) {
+            // get day of the week for currentDate
+            const day = currentDate.getDay();
+            // check if the day is in the frequency array
+            if (!parseResult.data.frequency[day]) {
+              currentDate.setDate(currentDate.getDate() + 1);
+              continue;
+            }
+          await prisma.post.create({
+            data: {
+              title: parseResult.data.title,
+              content: "",
+              created_at: currentDate,
+              created_from_topic: parseResult.data.item_type,
+              relevant_files: [],
+            },
+          });
+          currentDate.setDate(currentDate.getDate() + 7);
+        }
+
+
 
     return Response.json("Success", { status: 200 });
   } catch (error) {
