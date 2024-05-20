732522a3a0a287c7f2118495eb52800e321ffc98
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-20 14:04:35 -0500

    submit scheduled posts as props for the calendar

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -6,11 +6,24 @@ import prisma from '~/lib/server/prisma';
 export default async function DashboardPage() {
 
   const availableThemes = await prisma.themes.findMany();
+  const scheduledPostsForThisWeek = await prisma.post.findMany({
+    select: {
+      title: true,
+      schedule_date: true,
+    },
+    where: {
+      schedule_date: {
+        gte: new Date(),
+        lte: new Date(new Date().setDate(new Date().getDate() + 7)),
+      },
+    },
+  });
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
     <UserButton />
-    <CalendarTest />  
+    <CalendarTest events={scheduledPostsForThisWeek.map(item => ({ title: item.title!, date: item.schedule_date! }))} />
     <AddScheduler availableThemes={availableThemes} />
     </main>
   );
