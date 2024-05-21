e4b81f1f9c55e0f740344a07b38300ffd63cdff1
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-21 17:20:50 -0500

    add approved or pending label

--- src/components/ui/GridPost.tsx
+++ src/components/ui/GridPost.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import PostModal from "./PostModal";
 
 export default function GridPost({ post }: { post: post }) {
-  const { title, content, created_at } = post;
+  const { title, content, schedule_date, approved } = post;
   const [showEditModal, setShowEditModal] = useState(false);
 
   return (
@@ -17,8 +17,13 @@ export default function GridPost({ post }: { post: post }) {
         <div className="flex items-center justify-center gap-2.5">
           <Clock size={16} />
           <p className="text-sm font-medium text-black/50">
-            {created_at.toLocaleDateString("en-US")}
+            {schedule_date?.toLocaleDateString("en-US")}
           </p>
+          {approved ? (
+            <p className="text-sm font-medium text-green-500">Approved</p>
+          ) : (
+            <p className="text-sm font-medium text-red-500">Pending</p>
+          )}
         </div>
         <h1 className="text-sm font-semibold">{title}</h1>
         <p className="overflow-scroll whitespace-break-spaces text-xs font-normal text-black/50">
