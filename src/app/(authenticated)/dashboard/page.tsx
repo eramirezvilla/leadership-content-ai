299944950b786b036369e25ee80efa0d7b980db8
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-21 10:38:35 -0500

    add link to posts and styling update

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import CalendarTest from "~/components/ui/FullCalendar";
 import AddScheduler from "~/components/ui/AddScheduler";
 import prisma from "~/lib/server/prisma";
 import { type post } from "@prisma/client";
+import Link from "next/link";
 
 export default async function DashboardPage() {
   const availableThemes = await prisma.themes.findMany();
@@ -35,29 +36,35 @@ export default async function DashboardPage() {
   ).length;
 
   return (
-    <div className="flex w-full gap-4 px-8">
+    <div className="flex w-full gap-4 px-8 pt-4">
       <div className="min-h-screen flex-1 flex-col items-center justify-center">
         <CalendarTest events={scheduledPostsForThisWeek} />
       </div>
       <div className="w-full flex-1 items-center justify-center">
-          <div className="flex justify-end h-16">
-            <AddScheduler availableThemes={availableThemes} />
-          </div>
+        <div className="flex h-16 justify-end">
+          <AddScheduler availableThemes={availableThemes} />
+        </div>
         <div className="flex justify-center gap-8">
-        <div className="flex flex-col items-center justify-center border border-1 rounded-lg h-40 w-40 gap-2.5">
+          <Link href="/posts">
+          <div className="border-1 flex h-40 w-40 flex-col items-center justify-center gap-2.5 rounded-lg border">
             <div className="flex w-full flex-1 items-center justify-center">
               <h2 className="text-3xl font-bold">{amtPostsWithSchedule}</h2>
             </div>
             <div className="flex w-full flex-1 items-start justify-center">
-              <p className="text-md text-center font-medium whitespace-pre-wrap">Scheduled Posts</p>
+              <p className="text-md whitespace-pre-wrap text-center font-medium">
+                Scheduled Posts
+              </p>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-center border border-1 rounded-lg h-40 w-40 gap-2.5">
-          <div className="flex w-full flex-1 items-center justify-center">
-            <h2 className="text-3xl font-bold">{amtPostsNeedingApproval}</h2>
+          </Link>
+          <div className="border-1 flex h-40 w-40 flex-col items-center justify-center gap-2.5 rounded-lg border">
+            <div className="flex w-full flex-1 items-center justify-center">
+              <h2 className="text-3xl font-bold">{amtPostsNeedingApproval}</h2>
             </div>
             <div className="flex w-full flex-1 items-start justify-center">
-            <p className="text-md text-center font-medium whitespace-pre-wrap">Posts Pending Approval</p>
+              <p className="text-md whitespace-pre-wrap text-center font-medium">
+                Posts Pending Approval
+              </p>
             </div>
           </div>
         </div>
