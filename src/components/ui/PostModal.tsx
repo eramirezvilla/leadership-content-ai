59b00a06d1aded109cf9b9ffb5f1b0cb0c2c1558
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-21 17:28:49 -0500

    styling updates

--- src/components/ui/PostModal.tsx
+++ src/components/ui/PostModal.tsx
@@ -18,21 +18,33 @@ export default function PostModal({
   open,
   setOpen,
 }: PostModalProps) {
-  const { title, content, created_at, created_from_topic, relevant_files } =
+  const { title, content, created_at, created_from_topic, relevant_files, schedule_date, approved } =
     postToEdit;
 
   return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
-      <DialogHeader>
-        <DialogTitle className="max-w-prose">{title}</DialogTitle>
-      </DialogHeader>
-      <p className="text-sm font-medium text-black/50">{created_at.toLocaleDateString("en-US")}</p>
-        <p className="max-w-prose">{content}</p>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="max-w-prose">{title}</DialogTitle>
+        </DialogHeader>
+        <div className="flex gap-2 5">
+            <p className="text-sm font-bold text-black/50">Scheduled For: </p>
+            <p className="text-sm font-medium text-black/50">
+              {created_at.toLocaleDateString("en-US")}
+            </p>
+            {approved ? (
+            <p className="text-sm font-medium text-green-500">Approved</p>
+          ) : (
+            <p className="text-sm font-medium text-red-500">Pending</p>
+          )}
+        </div>
+        <div className="flex max-w-prose px-6 border border-1 py-4 rounded-lg">
+            <p className="text-sm">{content}</p>
+        </div>
         <p>Rel:{relevant_files}</p>
-      <DialogFooter>
-        <button onClick={() => setOpen(false)}>Close</button>
-      </DialogFooter>
+        <DialogFooter>
+          <button onClick={() => setOpen(false)}>Close</button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
