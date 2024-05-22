3fa4fe4cb3da4a3e2d66d473d9d9d0619dd3a3c7
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-22 14:34:51 -0500

    init layout

--- src/components/ui/CalendarPost.tsx
+++ src/components/ui/CalendarPost.tsx
@@ -8,8 +8,12 @@ interface CalendarPostProps {
 
 export default function CalendarPost({ post }: CalendarPostProps) {
 
+    const { title, schedule_date } = post
+
     return (
-        <>
-        </>
+        <div className="flex flex-col ">
+            <h1 className="text-sm text-brand_background">{title}</h1>
+            <p className="text-xs text-brand_background">{schedule_date?.toLocaleDateString("en-US")}</p>
+        </div>
     )
 }
