10120539299251b79e7e99cbc82f3e5b6f1b3a8d
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-22 14:35:01 -0500

    updated layout for calendar

--- src/app/(authenticated)/calendar/page.tsx
+++ src/app/(authenticated)/calendar/page.tsx
@@ -13,8 +13,15 @@ const postsWithSchedule = await prisma.post.findMany({
 export default function Calendar(){
 
     return (
-        <div className="flex w-full h-full justify-center items-center">
-        <CalendarTest events={postsWithSchedule} view="dayGridMonth"/>  
+        <div className="grid grid-cols-3 w-full h-full justify-center items-center px-4">
+        <div className="col-span-1">
+            <div className="flex flex-col">
+                <p> Something here </p>
+            </div>
+        </div>
+        <div className="col-span-2 w-full">
+            <CalendarTest events={postsWithSchedule} view="dayGridMonth"/>  
+        </div>
         </div>
     )
 }
