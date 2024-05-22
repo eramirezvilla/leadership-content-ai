4fb33c98bd79fd80bffdbe60ac8a1e3787ddd438
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-22 14:01:54 -0500

    new component to style posts on full calendar

---
+++ src/components/ui/CalendarPost.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useState } from "react"
+import type { post } from "@prisma/client"
+
+interface CalendarPostProps {
+    post: post
+}
+
+export default function CalendarPost({ post }: CalendarPostProps) {
+
+    return (
+        <>
+        </>
+    )
+}
