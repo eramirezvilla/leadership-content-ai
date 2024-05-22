afdcac4f0e7941033b56c75a05a84ce1a5b73725
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-22 14:01:43 -0500

    get events from db

--- src/app/(authenticated)/calendar/page.tsx
+++ src/app/(authenticated)/calendar/page.tsx
@@ -1,10 +1,20 @@
 import CalendarTest from "~/components/ui/FullCalendar"
+import prisma from "~/lib/server/prisma"
+
+const postsWithSchedule = await prisma.post.findMany({
+    where: {
+      schedule_date: {
+        not: null,
+      },
+    },
+  });
+
 
 export default function Calendar(){
 
     return (
         <div className="flex w-full h-full justify-center items-center">
-        {/* <CalendarTest />   */}
+        <CalendarTest events={postsWithSchedule} view="dayGridMonth"/>  
         </div>
     )
 }
