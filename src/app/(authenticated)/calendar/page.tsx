3faf072f11573a8b8d5e2315a1851e13d085227b
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-22 15:19:15 -0500

    updated layout

--- src/app/(authenticated)/calendar/page.tsx
+++ src/app/(authenticated)/calendar/page.tsx
@@ -1,5 +1,6 @@
 import CalendarTest from "~/components/ui/FullCalendar"
 import prisma from "~/lib/server/prisma"
+import AddScheduler from "~/components/ui/AddScheduler";
 
 const postsWithSchedule = await prisma.post.findMany({
     where: {
@@ -9,19 +10,23 @@ const postsWithSchedule = await prisma.post.findMany({
     },
   });
 
+  const availableThemes = await prisma.themes.findMany();
+
 
 export default function Calendar(){
 
     return (
-        <div className="grid grid-cols-3 w-full h-full justify-center items-center px-4">
-        <div className="col-span-1">
-            <div className="flex flex-col">
-                <p> Something here </p>
+        <div className="flex w-full py-2 px-4">
+            <div className="grid grid-cols-3 w-full h-full justify-center items-center py-2 px-4 border border-1 rounded-lg">
+            <div className="col-span-1 h-full px-6">
+                <div className="flex flex-col w-full h-full px-4 py-2 justify-start">
+                    <AddScheduler availableThemes={availableThemes}/>
+                </div>
             </div>
-        </div>
-        <div className="col-span-2 w-full">
-            <CalendarTest events={postsWithSchedule} view="dayGridMonth"/>  
-        </div>
+            <div className="col-span-2 w-full">
+                <CalendarTest events={postsWithSchedule} view="dayGridMonth"/>
+            </div>
+            </div>
         </div>
     )
 }
