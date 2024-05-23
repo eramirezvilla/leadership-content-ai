28b36b957b7c61f343320ca6e3892da947b13744
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-23 09:49:54 -0500

    add temporary placeholder where filters will be

--- src/app/(authenticated)/calendar/page.tsx
+++ src/app/(authenticated)/calendar/page.tsx
@@ -19,8 +19,10 @@ export default function Calendar(){
         <div className="flex w-full py-2 px-4">
             <div className="grid grid-cols-3 w-full h-full justify-center items-center py-2 px-4 border border-1 rounded-lg">
             <div className="col-span-1 h-full px-6">
-                <div className="flex flex-col w-full h-full px-4 py-2 justify-start">
+                <div className="flex flex-col w-full h-full pr-6 pl-2 py-2 justify-start gap-4">
                     <AddScheduler availableThemes={availableThemes}/>
+                    <div className="flex h-full w-full bg-gradient-to-b from-brand_background to-brand_secondary via-brand_primary rounded-lg">
+                    </div>
                 </div>
             </div>
             <div className="col-span-2 w-full">
