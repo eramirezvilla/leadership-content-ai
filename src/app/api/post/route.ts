ffb86fc78f469d7e413feb3bfcccfbe38ffd436e
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-28 17:43:28 -0500

    include theme name in post creation

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
     }
 
     const {
-      theme_name,
+      created_from_theme,
       industry_name,
       discussion_topic,
       topic_description,
@@ -31,7 +31,7 @@ export async function POST(req: Request) {
 
     const themeVals = await prisma.themes.findFirst({
       where: {
-        title: theme_name,
+        title: created_from_theme,
       },
     });
     if (!themeVals) {
@@ -103,7 +103,7 @@ export async function POST(req: Request) {
           title: discussion_topic,
           content: openaiResponse?.choices[0]?.message.content,
           created_from_topic: mapping_id,
-          created_from_theme: theme_name,
+          created_from_theme: created_from_theme,
           relevant_files: relevantFileIds,
           user_id: userId,
           schedule_date: schedule_date,
