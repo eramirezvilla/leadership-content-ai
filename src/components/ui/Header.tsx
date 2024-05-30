21b4f47bf284e09d3578e30ce5f4d9b1bd604f72
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-05-30 11:11:45 -0500

    include name next to user button

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,12 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
 
 export default function Header(){
-    
 
     return (
         <>
-        <div className="flex w-full justify-end px-12 py-6">
-            <UserButton />
+        <div className="flex w-full justify-end px-12 py-6 gap-2.5 items-center">
+            <UserButton showName={true}/>
         </div>
         <div className="border-b border-gray-200"></div>
         </>
