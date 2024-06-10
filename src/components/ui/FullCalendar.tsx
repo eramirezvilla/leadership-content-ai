1fb913036a7138dd0170c7d35990fb1117c95b2e
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-06-10 10:23:11 -0500

    pass images and files as props

--- src/components/ui/FullCalendar.tsx
+++ src/components/ui/FullCalendar.tsx
@@ -4,7 +4,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
 import GridPost from "./GridPost";
-import { type post } from "@prisma/client";
+import { type post, file} from "@prisma/client";
 import CalendarPost from "./CalendarPost";
 import { use, useEffect, useState } from "react";
 import LinkedInPost from "./LinkedInPost";
@@ -12,6 +12,8 @@ import LinkedInPost from "./LinkedInPost";
 interface CalendarTestProps {
   events: post[];
   view?: string;
+  allImages: Record<string, string[]>;
+  allFiles: file[];
 }
 
 interface EventWithStringId{
@@ -20,7 +22,20 @@ interface EventWithStringId{
   originalEvent: post;
 }
 
-export default function CalendarTest({ events, view }: CalendarTestProps) {
+function getImageUrlsForEachPost(postToGet: post, allFiles: file[], allImages: Record<string, string[]>) {
+  const imageUrls: string[] = [];
+  postToGet.relevant_files.forEach((file_id) => {
+    const file = allFiles.find((file) => file.id === file_id);
+    if (!file) return;
+    const urls = allImages[file.filename];
+    if (urls) {
+      imageUrls.push(...urls);
+    }
+  });
+  return imageUrls;
+}
+
+export default function CalendarTest({ events, view, allImages, allFiles }: CalendarTestProps) {
   const [eventsToDisplay, setEventsToDisplay] = useState<post[]>(events);
   const [eventsWithStringIds, setEventsWithStringIds] = useState<EventWithStringId[]>([]);
 
@@ -71,7 +86,7 @@ export default function CalendarTest({ events, view }: CalendarTestProps) {
       }}
       events={eventsWithStringIds}
       eventContent={function (arg) {
-        return renderEventContent(arg.event._def.extendedProps.originalEvent as post, view);
+        return renderEventContent(arg.event._def.extendedProps.originalEvent as post, allFiles, allImages, view);
       }}
       editable={false}
       droppable={false}
@@ -83,13 +98,15 @@ export default function CalendarTest({ events, view }: CalendarTestProps) {
   );
 }
 
-function renderEventContent(eventInfo: post, view?: string) {
+
+function renderEventContent(eventInfo: post, allFiles: file[], allImages: Record<string, string[]>, view?: string) {
   if (view === "dayGridMonth") {
     return <div className="flex flex-col w-fit overflow-hidden">
       <CalendarPost post={eventInfo} />
     </div>;
   }
+  const imageUrls = getImageUrlsForEachPost(eventInfo, allFiles, allImages);
   // return <GridPost post={eventInfo} />;
-  return <LinkedInPost post={eventInfo} />;
+  return <LinkedInPost post={eventInfo} allImages={imageUrls}/>;
   
 }
