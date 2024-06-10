dbd536a550ee97f9def91e92b08323dd1cb92d26
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-06-10 14:48:20 -0500

    validation for updates

--- src/lib/validation/ImageGenerator.tsx
+++ src/lib/validation/ImageGenerator.tsx
@@ -5,4 +5,9 @@ export const genImageSchema = z.object({
   post_content: z.string(),
 });
 
+export const updateImageSchema = z.object({
+  id: z.string(),
+  image_url: z.string(),
+});
+
 export type GenImageSchema = z.infer<typeof genImageSchema>;
