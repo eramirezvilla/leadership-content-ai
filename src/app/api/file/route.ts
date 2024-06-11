1c01eeb5f346e57a034c798c392145c348d60065
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-06-10 19:50:55 -0500

    parse param ids

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,49 +1,53 @@
 import { getRelevantFilesSchema } from "~/lib/validation/RelevantFile";
 import prisma from "~/lib/server/prisma";
 import { supabase } from "~/lib/server/supabase";
+import { z } from "zod";
 
 export async function GET(req: Request) {
-    try {
-      const url = new URL(req.url);
-      const query = Object.fromEntries(url.searchParams.entries());
-      const params = getRelevantFilesSchema.parse(query);
+  try {
+    const url = new URL(req.url);
+    const query = Object.fromEntries(url.searchParams.entries());
+    const params = getRelevantFilesSchema.parse(query);
 
-      const relevantFiles = await prisma.post.findMany({
-        select: {
-          relevant_files: true,
+    const idsArray = params.ids.split(",").map((id) => id.trim());
+
+    const idsSchema = z.array(z.string());
+    const validatedIds = idsSchema.parse(idsArray);
+
+    const relevantFiles = await prisma.post.findMany({
+      select: {
+        relevant_files: true,
+      },
+      where: {
+        id: {
+          in: validatedIds.map((id) => Number(id)),
         },
-        where: {
-          id: {
-            in: params.ids.map(id => Number(id)),
-          }
-        },
-      });
+      },
+    });
 
-        if (!relevantFiles) {
-            return Response.json("No relevant files found", { status: 404 });
-        }
-        
-        const filenames = await prisma.file.findMany({
-            select: {
-                filename: true,
-            },
-            where: {
-                id: {
-                    in: relevantFiles.map(file => Number(file.relevant_files)),
-                },
-            },
-        });
-
-        const downloadLinks = filenames.map(file => {
-            return supabase.storage.from("testing-specs").getPublicUrl(file.filename)
-            .data.publicUrl;
-        }
-        );
-
-        return Response.json(downloadLinks, { status: 200 });
-
-    } catch (error) {
-        console.error(error);
-        return Response.json("An error occurred", { status: 500 });
-        }
+    if (!relevantFiles) {
+      return Response.json("No relevant files found", { status: 404 });
     }
+
+    const filenames = await prisma.file.findMany({
+      select: {
+        filename: true,
+      },
+      where: {
+        id: {
+          in: relevantFiles.map((file) => Number(file.relevant_files)),
+        },
+      },
+    });
+
+    const downloadLinks = filenames.map((file) => {
+      return supabase.storage.from("testing-specs").getPublicUrl(file.filename)
+        .data.publicUrl;
+    });
+
+    return Response.json(downloadLinks, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json("An error occurred", { status: 500 });
+  }
+}
