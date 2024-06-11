043ee928c9cafdc6b7a783795a10bad922104eb9
Edgar Ramirez <eramirezvilla@sandiego.edu>
2024-06-10 19:20:59 -0500

    add api route for relevant files

---
+++ src/app/api/file/route.ts
@@ -0,0 +1,49 @@
+import { getRelevantFilesSchema } from "~/lib/validation/RelevantFile";
+import prisma from "~/lib/server/prisma";
+import { supabase } from "~/lib/server/supabase";
+
+export async function GET(req: Request) {
+    try {
+      const url = new URL(req.url);
+      const query = Object.fromEntries(url.searchParams.entries());
+      const params = getRelevantFilesSchema.parse(query);
+
+      const relevantFiles = await prisma.post.findMany({
+        select: {
+          relevant_files: true,
+        },
+        where: {
+          id: {
+            in: params.ids.map(id => Number(id)),
+          }
+        },
+      });
+
+        if (!relevantFiles) {
+            return Response.json("No relevant files found", { status: 404 });
+        }
+        
+        const filenames = await prisma.file.findMany({
+            select: {
+                filename: true,
+            },
+            where: {
+                id: {
+                    in: relevantFiles.map(file => Number(file.relevant_files)),
+                },
+            },
+        });
+
+        const downloadLinks = filenames.map(file => {
+            return supabase.storage.from("testing-specs").getPublicUrl(file.filename)
+            .data.publicUrl;
+        }
+        );
+
+        return Response.json(downloadLinks, { status: 200 });
+
+    } catch (error) {
+        console.error(error);
+        return Response.json("An error occurred", { status: 500 });
+        }
+    }
